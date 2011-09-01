d55cb8615914d8ce9f294d61d8dc39bd36bdcc92
skye.book@gmail.com <skye.book@gmail.com@aed54743-0274-458c-b270-e677e25d1059>
2011-09-01 04:26:04 +0000

    Initial commit of the jME2 asset loader plugin

---
+++ src/net/skyebook/jme2loader/JME2AssetManagerConfigurator.java
@@ -0,0 +1,16 @@
+package net.skyebook.jme2loader;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.gde.core.assets.AssetManagerConfigurator;
+
+/**
+ *
+ * @author Skye Book
+ */
+@org.openide.util.lookup.ServiceProvider(service = AssetManagerConfigurator.class)
+public class JME2AssetManagerConfigurator implements AssetManagerConfigurator {
+    @Override
+    public void prepareManager(AssetManager manager) {
+        manager.registerLoader(net.skyebook.jme2loader.JME2Loader.class, "jme", "jme.xml");
+    }
+}
---
+++ src/net/skyebook/jme2loader/JME2LoaderDataObject.java
@@ -0,0 +1,32 @@
+package net.skyebook.jme2loader;
+
+import com.jme3.gde.core.assets.SpatialAssetDataObject;
+import java.io.IOException;
+import org.openide.filesystems.FileObject;
+import org.openide.loaders.DataObjectExistsException;
+import org.openide.loaders.MultiFileLoader;
+
+/**
+ * 
+ * @author Skye Book
+ */
+public class JME2LoaderDataObject extends SpatialAssetDataObject {
+
+    public JME2LoaderDataObject(FileObject pf, MultiFileLoader loader) throws DataObjectExistsException, IOException {
+        super(pf, loader);
+        registerEditor("application/jme", true);
+    }
+    
+    /*
+
+    @MultiViewElement.Registration(displayName = "#LBL_JME2Loader_EDITOR",
+    iconBase = "net/skyebook/jme2loader/monkey.gif",
+    mimeType = "application/jme",
+    persistenceType = TopComponent.PERSISTENCE_ONLY_OPENED,
+    preferredID = "JME2Loader",
+    position = 1000)
+    public static MultiViewEditorElement createEditor(Lookup lkp) {
+        return new MultiViewEditorElement(lkp);
+    
+    */
+}
